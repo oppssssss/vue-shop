187c303010491b88ad1be40189079b4e9eced53a
gaoxuhui <gaoxuhui.no1@gmail.com>
2020-04-08 23:41:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  @import "./assets/css/base.css"
+</style>
